0458f5dc8e02bb2af15b04c0a980efb45b040e07
bangjur <azzuriptr@gmail.com>
2024-09-26 15:16:40 +0700

    versi 1

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/hobi')
+def hobi():
+    return render_template('hobi.html')
+
+@app.route('/pekerjaan')
+def pekerjaan():
+    return render_template('pekerjaan.html')
+
+@app.route('/kontak')
+def kontak():
+    return render_template('kontak.html')
+
+@app.route('/pendidikan')
+def pendidikan():
+    return render_template('pendidikan.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
